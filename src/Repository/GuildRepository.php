69b552c6c5793354e66f67375cd6e3836a0ce2f1
kidthales <kidthales@agogpixel.com>
2025-04-06 13:49:38 -0700

    Implement guild model & adjust user model.

---
+++ src/Repository/GuildRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\Guild;
+use App\Repository\Traits\Alterable;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Guild>
+ */
+class GuildRepository extends ServiceEntityRepository
+{
+    use Alterable;
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Guild::class);
+    }
+
+    /**
+     * @param int|string $discordId
+     * @return Guild|null
+     */
+    public function findOneByDiscordId(int|string $discordId): ?Guild
+    {
+        return $this->createQueryBuilder('g')
+            ->andWhere('g.discordId = :val')
+            ->setParameter('val', $discordId)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+}
