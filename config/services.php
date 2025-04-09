30a5034306dd56ef5f93568c3059ab1766adfc09
kidthales <kidthales@agogpixel.com>
2025-04-08 23:22:56 -0700

    Update app services config to PHP; wire sensitive Discord data from env vars to service container parameters.

---
+++ config/services.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+use App\DependencyInjection\Parameters;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $container): void {
+    $parameters = $container->parameters();
+
+    $parameters
+        ->set(Parameters::DEFAULT_STRING_PARAMETER, '!ChangeThisParameterValue!')
+        ->set(
+            Parameters::DISCORD_APP_PUBLIC_KEY,
+            '%env(default:' . Parameters::DEFAULT_STRING_PARAMETER . ':string:DISCORD_APP_PUBLIC_KEY)%'
+        )
+        ->set(
+            Parameters::DISCORD_OAUTH2_CLIENT_ID,
+            '%env(default:' . Parameters::DEFAULT_STRING_PARAMETER . ':string:DISCORD_OAUTH2_CLIENT_ID)%'
+        )
+        ->set(
+            Parameters::DISCORD_OAUTH2_CLIENT_SECRET,
+            '%env(default:' . Parameters::DEFAULT_STRING_PARAMETER . ':string:DISCORD_OAUTH2_CLIENT_SECRET)%'
+        );
+
+    $services = $container->services();
+
+    $services->defaults()
+        ->autowire()
+        ->autoconfigure();
+
+    $services->load('App\\', __DIR__ . '/../src/')
+        ->exclude([
+            __DIR__ . '/../src/DependencyInjection/',
+            __DIR__ . '/../src/Entity/',
+            __DIR__ . '/../src/Kernel.php',
+        ]);
+
+    $services->set('custom_normalizer', \Symfony\Component\Serializer\Normalizer\CustomNormalizer::class)
+        ->tag('serializer.normalizer');
+
+    $parameters->set('.container.dumper.inline_factories', true);
+};
